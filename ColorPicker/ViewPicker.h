87be36f9cb668c1fc68d3cdedcf32cd281786171
“Dolores <doloresmtz@gmail.com>
2015-02-02 11:56:36 -0600

    Selector de color RGB  y  HEX usando spinner, stepper

--- ColorPicker/ViewPicker.h
+++ ColorPicker/ViewPicker.h
@@ -10,22 +10,37 @@
 
 @interface ViewPicker : UIViewController
 
+@property (strong, nonatomic) NSTimer *timer;
 @property (strong, nonatomic) IBOutlet UILabel *lbColor;
 @property (strong, nonatomic) IBOutlet UILabel *lbRed;
 @property (strong, nonatomic) IBOutlet UILabel *lbGreen;
 @property (strong, nonatomic) IBOutlet UILabel *lbBlue;
+@property (strong, nonatomic) IBOutlet UILabel *lbAlpha;
 @property (strong, nonatomic) IBOutlet UILabel *lbBlink;
+
 @property (strong, nonatomic) IBOutlet UITextField *txtColor;
 
 @property (strong, nonatomic) IBOutlet UISlider *slRed;
 @property (strong, nonatomic) IBOutlet UISlider *slGreen;
 @property (strong, nonatomic) IBOutlet UISlider *slBlue;
-@property (strong, nonatomic) IBOutlet UISlider *slBlink;
+@property (strong, nonatomic) IBOutlet UISlider *slAlpha;
+
+
+
+
+@property (strong, nonatomic) IBOutlet UIStepper *stBlink;
 
 - (IBAction)changeGreen:(id)sender;
 - (IBAction)changeBlue:(id)sender;
 - (IBAction)changeRed:(id)sender;
-- (IBAction)changeBlink:(id)sender;
+- (IBAction)changeAlpha:(id)sender;
+
+
+
+
+
+- (IBAction)clicBlink:(id)sender;
+
 
 
 @end
--- ColorPicker/ViewPicker.m
+++ ColorPicker/ViewPicker.m
@@ -11,7 +11,11 @@ int rcolor=0;
 int gcolor=0;
 int bcolor=0;
 float alpha=1;
-int blink=0;
+
+int blink=1;
+int tiempo=0;
+int segundo=0;
+
 UIColor *color;
 NSString *hex;
 @interface ViewPicker ()
@@ -29,33 +33,36 @@ NSString *hex;
     // Dispose of any resources that can be recreated.
 }
 
-void setColor(int rcol,int gcol,int bcol, float bk){
+void setColor(int rcol,int gcol,int bcol, float al){
     rcolor=rcol;
     gcolor=gcol;
     bcolor=bcol;
-    blink=bk;
+    alpha=al;
     color=[UIColor colorWithRed:rcolor/255.0
                           green:gcolor/255.0
                            blue:bcolor/255.0
-                          alpha:bk];
+                          alpha:al];
     
 }
 
 -(void) refreshColor{
     //Asi se llama a un action
     //[self changeGreen:nil];
+    
+    alpha=self.slAlpha.value;
+    alpha=(double)alpha/100;
+    self.lbAlpha.text=[NSString stringWithFormat:@"%.2f", alpha];
+    
     gcolor=self.slGreen.value;
     self.lbGreen.text=[@(gcolor) stringValue];
     
     bcolor=self.slBlue.value;
     self.lbBlue.text=[@(bcolor) stringValue];
     
-    blink=self.slBlink.value;
-    self.lbBlink.text=[@(blink) stringValue];
-    
     rcolor=self.slRed.value;
     self.lbRed.text=[@(rcolor) stringValue];
-    setColor(rcolor, gcolor, bcolor, blink);
+    setColor(rcolor, gcolor, bcolor, alpha);
+    
     
     self.lbColor.backgroundColor=color;
     
@@ -80,12 +87,46 @@ void setColor(int rcol,int gcol,int bcol, float bk){
     self.txtColor.text=hex;
     
 }
+
+- (void)startTimer {
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                                  target:self
+                                                selector:@selector(initBlink:)
+                                                userInfo:nil
+                                                 repeats:YES];
+    
+}
+
+- (void)initBlink:(NSTimer *)timer {
+    segundo++;
+    if (segundo==tiempo){
+        [self.timer invalidate];
+        self.timer = nil;
+        setColor(rcolor, gcolor, bcolor, alpha);
+    }else{
+    if((segundo % 2) == 0){
+        setColor(255, 255, 255, 255);
+    }else{
+        setColor(rcolor, gcolor, bcolor, alpha);
+    }
+    }
+    [self refreshColor];
+}
+
 -(NSString*) formatHex:(NSString*)hex{
     int len = [hex length];
     if(len==1){
         hex = [NSString stringWithFormat:@"%@%@",@"0",hex ];
     }
     return hex;
+}
+
+- (IBAction)changeRed:(id)sender {
+    [self refreshColor];
+}
+
+- (IBAction)changeAlpha:(id)sender {
+    [self refreshColor];
 }
 
 - (IBAction)changeGreen:(id)sender {
@@ -96,11 +137,13 @@ void setColor(int rcol,int gcol,int bcol, float bk){
     [self refreshColor];
 }
 
-- (IBAction)changeBlink:(id)sender {
-    [self refreshColor];
+- (IBAction)clicBlink:(id)sender {
+    self.lbBlink.text=[NSString stringWithFormat:@"%d",(int)self.stBlink.value];
+    blink=self.stBlink.value;
 }
 
-- (IBAction)changeRed:(id)sender {
-   [self refreshColor];
-}
+
+
+
+
 @end
